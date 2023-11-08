78758a6de0168d3c76c46eca661710df559da4a7
Ali Mughal <57093853+alemughal@users.noreply.github.com>
2023-11-08 22:36:26 +0500

    Modifying Code

--- Exercise 19/app.ts
+++ Exercise 19/app.ts
@@ -1,5 +1,15 @@
-// List of languages
+// Define nested arrays for mountains, rivers, countries, cities, and languages
+let data: string[][] = [
+    ["Mount Everest", "K2", "Kangchenjunga", "Lhotse", "Makalu"],
+    ["Nile", "Amazon", "Yangtze", "Mississippi", "Ganges"],
+    ["United States", "China", "India", "Brazil", "Russia"],
+    ["New York", "Tokyo", "London", "Mumbai", "Beijing"],
+    ["English", "Mandarin", "Spanish", "Hindi", "Arabic"]
+];
 
-const mountains: string[] = ["Mount Everest", "K2", "Kangchenjunga", "Lhotse", "Makalu"];
-
-console.log(mountains)
+// Accessing and printing the nested arrays
+console.log("Mountains: ", data[0]);
+console.log("Rivers: ", data[1]);
+console.log("Countries: ", data[2]);
+console.log("Cities: ", data[3]);
+console.log("Languages: ", data[4]);
