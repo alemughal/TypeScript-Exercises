d5f7213876f7a84effe33fcc4b7d6ebd5f24814e
Ali Mughal <57093853+alemughal@users.noreply.github.com>
2023-11-08 18:21:53 +0500

    Modifying code

--- Exercise 7/app.ts
+++ Exercise 7/app.ts
@@ -1,5 +1,6 @@
-console.log(4 + 4);
-console.log(12 - 4);
-console.log(4 * 2);
-console.log(16 / 2);
+// You should create four lines that look like this: console.log(5 + 3)
 
+console.log(5 + 3);
+console.log(2 + 6);
+console.log(10 - 2);
+console.log(16 / 2);
